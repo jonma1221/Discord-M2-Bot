d4a781dcfd37e7feed666a31d29e326f5ec389da
Jon <jonma1221@gmail.com>
2026-08-31 14:39:22 -0700

    Remove unused requirements

--- open_ai_text_prompt.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client = OpenAI()
-
-response = client.responses.create(
-    model="gpt-5.2",
-    input="This is a test prompt from a python script."
-)
-
-print(response.output_text)
